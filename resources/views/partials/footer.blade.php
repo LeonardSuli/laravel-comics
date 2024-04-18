bd98d047b6c4c0a474cc07642a0bcd54dacb479a
LeonardSuli <leonardsuli1991@gmail.com>
2024-04-18 18:04:39 +0200

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,87 @@
-<footer>Footer</footer>
+<footer>
+       
+    <div class="footer_top">
+      
+      <div class="container d-flex">
+
+          <div class="row">
+
+            <div class="col">
+
+              <h4>DC COMICS</h4>
+              <ul>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+              </ul>
+
+              <h4>SHOP</h4>
+              <ul>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+              </ul>
+
+            </div>
+
+            <div class="col">
+
+              <h4>DC</h4>
+              <ul>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy policy (New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent Workshops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+
+            </div>
+
+            <div class="col">
+
+              <h4>SITES</h4>
+              <ul>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+              </ul>
+
+            </div>
+
+          </div>
+
+      </div>
+      
+    </div>
+
+    <div class="footer_bottom">
+
+      <div class="container d-flex">
+        
+        <button class="sign_up">SIGN-UP NOW!</button>
+        
+        <div class="social">
+
+          <span>FOLLOW US</span>
+          <img src="{{ Vite::asset('resources/images/footer-facebook.png') }}">
+          <img src="{{ Vite::asset('resources/images/footer-twitter.png') }}">
+          <img src="{{ Vite::asset('resources/images/footer-youtube.png') }}">
+          <img src="{{ Vite::asset('resources/images/footer-pinterest.png') }}">
+          <img src="{{ Vite::asset('resources/images/footer-periscope.png') }}">
+        </div>
+
+      </div>
+
+    </div>
+
+  </footer>
