e01173bafcf74e9f27bc1a612b2912d0cc4924e5
LeonardSuli <leonardsuli1991@gmail.com>
2024-04-20 10:56:04 +0200

    add data in folder config

--- resources/views/guests/single.blade.php
+++ resources/views/guests/single.blade.php
@@ -2,6 +2,43 @@
 
 @section('content')
 
-<h1>Single page</h1>
+<div class="container-single">
+    <h1>Single page</h1>
+    <img src="{{ $comics[0]['thumb'] }}" alt="">
+    
+</div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{{-- @foreach ($comics as $comic)
+
+        <div class="col-2">
+
+            <div class="card">
+
+                <img src="{{ $comic['thumb'] }}" alt="">
+                <h4>{{ $comic['series'] }}</h4>
+                <span>{{ $comic['price'] }}</span>
+        
+            </div>
+            
+        </div>
+        
+        @endforeach --}}
 @endsection
